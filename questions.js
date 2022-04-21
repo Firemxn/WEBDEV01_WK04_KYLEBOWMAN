6b7120b1ccafb1901be1ff20b3ddf348a133241f
KB Firemxn <Firemxn@KBs-MacBook-Pro.local>
2022-04-21 16:03:50 -0500

    Added the questions

---
+++ questions.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        type: 'list',
+        name: 'Q1',
+        message: 'What year was basketball invented?',
+        choices: [
+            "1902",
+            "1886",
+            "1891",
+            "1897",
+
+        ]
+    }
+]
